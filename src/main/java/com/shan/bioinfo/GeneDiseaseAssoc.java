7462abdc626a988a9c55ff197826cd3cdef1eeea
Shantanu Godbole <sgodbole@PC114-33.cise.ufl.edu>
2015-10-09 19:55:51 -0400

    fsdklfljds

---
+++ src/main/java/com/shan/bioinfo/GeneDiseaseAssoc.java
@@ -0,0 +1,99 @@
+package com.shan.bioinfo;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+public class GeneDiseaseAssoc {
+
+	private static final String APPLICATION_NAME =
+	        "Drive API Java Quickstart";
+
+	    /** Directory to store user credentials for this application. */
+	    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+	        System.getProperty("user.home"), ".credentials/drive-java-quickstart");
+
+	    /** Global instance of the {@link FileDataStoreFactory}. */
+	    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+	    /** Global instance of the JSON factory. */
+	    private static final JsonFactory JSON_FACTORY =
+	        JacksonFactory.getDefaultInstance();
+
+	    /** Global instance of the HTTP transport. */
+	    private static HttpTransport HTTP_TRANSPORT;
+
+	    /** Global instance of the scopes required by this quickstart. */
+	    private static final List<String> SCOPES =
+	        Arrays.asList(DriveScopes.DRIVE_METADATA_READONLY);
+	    
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		URL url = new URL("https://drive.google.com/open?id=0B-F1_EKfc_rOWVNpMGM1SFhQSDg");
+        HttpURLConnection connection = (HttpURLConnection) url
+                .openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("GET");
+        InputStreamReader str = new InputStreamReader(connection.getInputStream());
+		//FileReader fr = new FileReader(new File("C:\\Users\\SHANDEMETZ\\Downloads\\Compressed\\gene_association.goa_human\\gene_association.goa_human"));
+		BufferedReader br = new BufferedReader(str);
+		String line = "";
+		while((line = br.readLine()) != null){
+			System.out.println(line);
+			
+		}
+		  File file = service.files().get("0B-F1_EKfc_rOWVNpMGM1SFhQSDg").execute();
+
+	      System.out.println("Title: " + file.getTitle());
+	      System.out.println("Description: " + file.getDescription());
+	      System.out.println("MIME type: " + file.getMimeType());
+	    
+		}
+	public static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in =
+            DriveQuickstart.class.getResourceAsStream("/client_secret.json");
+        GoogleClientSecrets clientSecrets =
+            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(DATA_STORE_FACTORY)
+                .setAccessType("offline")
+                .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+            flow, new LocalServerReceiver()).authorize("user");
+        System.out.println(
+                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
+    }
+		
+	}
+
+
