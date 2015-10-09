3a23ff6ca6a94cc5c5bd048d45f28c4694b20c1d
Shantanu Godbole <sgodbole@PC114-33.cise.ufl.edu>
2015-10-09 18:48:11 -0400

    changes for file in google drive

--- com/shan/bioinfo/GeneDiseaseAssoc.java
+++ com/shan/bioinfo/GeneDiseaseAssoc.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,13 +15,17 @@ public class GeneDiseaseAssoc {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		FileReader fr = new FileReader(new File("C:\\Users\\SHANDEMETZ\\Downloads\\Compressed\\gene_association.goa_human\\gene_association.goa_human"));
-		BufferedReader br = new BufferedReader(fr);
+		URL url = new URL("https://drive.google.com/open?id=0B-F1_EKfc_rOWVNpMGM1SFhQSDg");
+        HttpURLConnection connection = (HttpURLConnection) url
+                .openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("GET");
+        InputStreamReader str = new InputStreamReader(connection.getInputStream());
+		//FileReader fr = new FileReader(new File("C:\\Users\\SHANDEMETZ\\Downloads\\Compressed\\gene_association.goa_human\\gene_association.goa_human"));
+		BufferedReader br = new BufferedReader(str);
 		String line = "";
 		while((line = br.readLine()) != null){
-			if(line.contains("ATP7B")){
-				System.out.println(line);
-			}
+			System.out.println(line);
 		}
 			
 		}
